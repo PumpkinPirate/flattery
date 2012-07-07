4ff5ff663e06cef8d50d93aea241f26d49da53c5
Andrew Stoneman <andrew@geomancer.(none)>
2012-07-07 15:50:01 -0700

    Adding support for textures to SVG export

--- flattery/svgexport.rb
+++ flattery/svgexport.rb
@@ -38,6 +38,13 @@ module Flattery
             return
         end
         
+        has_textures = false
+        file_dir = File.dirname(file_path)
+        texture_dir = File.basename(file_path) + "-textures"
+        last_index = -1
+        pattern_number = 0
+        tw = Sketchup.create_texture_writer()
+        
         if container?(entity)
             f = File.new(file_path,  "w+")
             
@@ -68,15 +75,11 @@ module Flattery
             height = max_y - min_y + 2*margin
         
             transformation = Geom::Transformation.new([margin - min_x, margin - min_y, 0]) * transformation
-        
-            f.puts('<?xml version="1.0" standalone="no"?>')
-            f.puts('<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">')
-            f.puts("<svg width=\"#{width}in\" height=\"#{height}in\" viewBox=\"0 0 #{width} #{height}\"")
-            f.puts("     version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">")
             
             int_lines = []
             ext_lines = []
             faces = []
+            patterns = []
             
             get_entities(entity).each do |e|
                 if edge?(e)
@@ -99,16 +102,82 @@ module Flattery
                     end
                     points = points.join(" ")
                     if e.material
-                        c = e.material.color
-                        color = "rgb(#{c.red},#{c.green},#{c.blue})"
+                        if e.material.texture
+                            index = tw.load(e, true)
+                            texture_name = File.join(texture_dir, "#{index}.png")
+                            
+                            if index > last_index
+                                last_index = index
+                                
+                                if !has_textures
+                                    has_textures = true
+                                    #FileUtils.mkdir(File.join(file_dir, texture_dir))
+                                end
+                                
+                                tw.write(e, true, File.join(file_dir, texture_name))
+                            end
+                            
+                            #     t            xy             uv
+                            #                                         -1
+                            # [a c d 0]   [x1 x2 x3 0]   [u1 u2 u3 0]
+                            # [b e f 0] = [y1 y2 y3 0] * [v1 v2 v3 0]
+                            # [0 0 1 0]   [1  1  1  0]   [1  1  1  0]
+                            # [0 0 0 1]   [1  1  1  1]   [1  1  1  1]
+                            
+                            uv_helper = e.get_UVHelper(true, false, tw)
+                            xy_points = []
+                            uv_points = []
+                            for i in 0..2
+                                xyz = e.vertices[i].position
+                                xy_points.push(xyz.transform(transformation))
+                                uv_points.push(uv_helper.get_front_UVQ(xyz))
+                            end
+                            
+                            xy = Geom::Transformation.new([xy_points[0].x, xy_points[0].y, 1, 1,
+                                                           xy_points[1].x, xy_points[1].y, 1, 1,
+                                                           xy_points[2].x, xy_points[2].y, 1, 1,
+                                                           0, 0, 0, 1])
+                            
+                            # Scaling here combined with the width and height in the SVG
+                            # get firefox to stop pixilating the textures.
+                            uv = Geom::Transformation.new([10*uv_points[0].x, 10 - 10*uv_points[0].y, 1, 1,
+                                                           10*uv_points[1].x, 10 - 10*uv_points[1].y, 1, 1,
+                                                           10*uv_points[2].x, 10 - 10*uv_points[2].y, 1, 1,
+                                                           0, 0, 0, 1])
+                            
+                            t = xy * uv.inverse
+                            t = t.to_a
+                            svg_matrix = "matrix(#{t[0]}, #{t[1]}, #{t[4]}, #{t[5]}, #{t[8]}, #{t[9]})"
+                            patterns.push("<pattern id=\"texture#{pattern_number}\" patternUnits=\"userSpaceOnUse\"")
+                            patterns.push("         x=\"0\" y=\"0\" width=\"10\" height=\"10\" viewBox=\"0 0 1 1\"")
+                            patterns.push("         patternTransform=\"#{svg_matrix}\">")
+                            patterns.push("  <image xlink:href=\"#{texture_name}\" x=\"0\" y=\"0\" ")
+                            patterns.push("         width=\"1\" height=\"1\" preserveAspectRatio=\"none\"/>")
+                            patterns.push("</pattern>")
+                            
+                            fill = "url('#texture#{pattern_number}')"
+                            
+                            pattern_number += 1
+                        else
+                            c = e.material.color
+                            fill = "rgb(#{c.red},#{c.green},#{c.blue})"
+                        end
                     else
-                        color = "rgb(255,255,255)"
+                        fill = "rgb(255,255,255)"
                     end
-                    faces.push("<polygon points=\"#{points}\" fill=\"#{color}\"/>")
+                    faces.push("<polygon points=\"#{points}\" fill=\"#{fill}\"/>")
                 end
             end
             
+            f.puts('<?xml version="1.0" standalone="no"?>')
+            f.puts('<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">')
+            f.puts("<svg width=\"#{width}in\" height=\"#{height}in\" viewBox=\"0 0 #{width} #{height}\"")
+            f.puts("     version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">")
+            
             f.puts('<g id="faces">')
+            f.puts('<defs>')
+            f.puts(patterns.join("\n"))
+            f.puts('</defs>')
             f.puts(faces.join("\n"))
             f.puts('</g>')
             
